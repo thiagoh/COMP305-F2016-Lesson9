58747b2057d1bc3e491d601507f20ff67a4c7a3b
Thiago Andrade <thiagoh@gmail.com>
2016-11-12 16:48:29 -0500

    first commit

---
+++ Assets/_Scripts/PlayerShooting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+
+	// PUBLIC VARIABLES FOR TESTING
+	public Transform FlashPoint;
+	public GameObject MuzzleFlash;
+	public AudioSource RifleShotSound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame (for Physics)
+	void FixedUpdate () {
+		if (Input.GetButtonDown ("Fire1")) {
+			// show the MuzzleFlash at the FlashPoint without any rotation
+			Instantiate (this.MuzzleFlash, this.FlashPoint.position, Quaternion.identity);
+
+			// Play Rifle Sound
+			this.RifleShotSound.Play();
+		}
+	}
+}
