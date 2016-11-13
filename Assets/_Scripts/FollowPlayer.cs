3d36ee4cc7ef30a9484bb0a7bf78e298e0b8ac51
Thiago Andrade <thiagoh@gmail.com>
2016-11-12 19:26:49 -0500

    terrain with barrels following the player

---
+++ Assets/_Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+
+    private Transform _transform;
+
+    // Use this for initialization
+    void Start() {
+
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        float step = speed * Time.deltaTime;
+        _transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
